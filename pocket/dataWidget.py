c5f47ea0d96925e01b98d895f402aa744f9d58c3
Andrej Perfilov [office] <33530333+p3rfilov@users.noreply.github.com>
2018-01-10 18:12:02 +0000

    edited dataWidget and mainWindow

--- pocket/dataWidget.py
+++ pocket/dataWidget.py
@@ -1,36 +1,65 @@
 from PyQt5.QtWidgets import QWidget, QLabel, QPushButton, QHBoxLayout, QVBoxLayout, QLayout, QTextEdit
+from PyQt5 import QtCore
 
 class dataWidget(QWidget):
-    def __init__(self, data={}):
+    def __init__(self):
         super().__init__()
-        self.params =   {
-                        'title':'New Note',
-                        'expanded':'False',
-                        'data':'None'
-                        }
-        self.params.update(data)
+        self.defaults = {'title':'New Note', 'data':'None'}
+        #self.heightMin = 35
+        #self.heightMax = 100
+        #self.resize(QtCore.QSize(self.sizeHint().width(), self.heightMin))
         
-        self.title = QLabel(self.params['title'])
-        
-        self.button = QPushButton('-')
-        self.button.setMaximumSize(17, 17)
-        self.button.clicked.connect(self.setTextHidden)
-        
-        self.textEdit = QTextEdit(self.params['data'])
+        self.title = QLabel(self.defaults['title'])
+        self.delButton = QPushButton('x')
+        self.delButton.setMaximumSize(17, 17)
+        self.delButton.hide()
+        self.textEdit = QTextEdit(self.defaults['data'])
         self.textEdit.hide()
+        self.editButton = QPushButton('edit')
+        self.editButton.setMaximumSize(35, 20)
+        self.editButton.hide()
         
         self.layout1 = QVBoxLayout()
         self.layout2 = QHBoxLayout()
+        self.layout3 = QHBoxLayout()
+        
         self.layout2.addWidget(self.title)
         self.layout2.addStretch()
-        self.layout2.addWidget(self.button)
+        self.layout2.addWidget(self.delButton)
+        
+        self.layout3.addStretch()
+        self.layout3.addWidget(self.editButton)
+        
         self.layout1.addLayout(self.layout2)
         self.layout1.addWidget(self.textEdit)
+        self.layout1.addLayout(self.layout3)
         self.layout2.setSizeConstraint(QLayout.SetFixedSize)
         self.setLayout(self.layout1)
         
-    def setTextHidden(self):
-        if self.textEdit.isHidden():
-            self.textEdit.show()
+        
+    def toggleWidgetTextField(self, hide=False):
+        if not hide:
+            if self.textEdit.isHidden():
+                self.textEdit.show()
+                self.delButton.show()
+                self.editButton.show()
+                self.resize(QtCore.QSize(0,0))
+                #self.resize(QtCore.QSize(self.sizeHint().width(), self.heightMax))
+            else:
+                self.textEdit.hide()
+                self.delButton.hide()
+                self.editButton.hide()
+                self.resize(QtCore.QSize(0,0))
+                #self.resize(QtCore.QSize(self.sizeHint().width(), self.heightMin))
         else:
             self.textEdit.hide()
+            self.delButton.hide()
+            self.editButton.hide()
+            
+    def setTitle(self, title):
+        self.title.setText(title)
+        
+    def setData(self, data):
+        self.textEdit.setText(data)
+        
+    
--- pocket/mainWindow.py
+++ pocket/mainWindow.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QMainWindow, QListWidgetItem
+from PyQt5.QtWidgets import QMainWindow, QListWidgetItem, QListWidget
 from PyQt5.uic import loadUi
 from dataWidget import dataWidget
 
@@ -8,19 +8,41 @@ class mainWindow(QMainWindow):
         self.ui = loadUi('mainWindow.ui')
         self.ui.show()
         
+        self.ui.list_items.setSizeAdjustPolicy(QListWidget.AdjustToContents)
+        self.ui.list_items.itemClicked.connect(self.expandCurrentItem)
+        
         for i in range(5):
-            itemData =  {
-                        'title':'Item ' + str(i),
-                        'expanded':'False',
-                        'data':str(i)*30
-                        }
+            listItem = QListWidgetItem()
+            widget = dataWidget()
+            widget.setTitle('Note ' + str(i))
+            widget.setData(str(i)*10)
+            listItem.setSizeHint(widget.sizeHint())
+            self.ui.list_items.addItem(listItem)
+            self.ui.list_items.setItemWidget(listItem, widget)
             
-            self.widget = dataWidget(itemData)
-            self.listItem = QListWidgetItem()
-            self.listItem.setSizeHint(self.widget.sizeHint())
-            self.ui.list_items.addItem(self.listItem)
-            self.ui.list_items.setItemWidget(self.listItem, self.widget)
+            widget.delButton.clicked.connect(self.removeRow)
             
+        
+    def expandCurrentItem(self):
+        self.closeInactiveItems()
+        item = self.ui.list_items.currentItem()
+        widget = self.ui.list_items.itemWidget(item)
+        widget.toggleWidgetTextField()
+        item.setSizeHint(widget.sizeHint())
+        
+    def closeInactiveItems(self):
+        count = self.ui.list_items.count()
+        for row in range(count):
+            if row != self.ui.list_items.currentRow():
+                item = self.ui.list_items.item(row)
+                widget = self.ui.list_items.itemWidget(item)
+                widget.toggleWidgetTextField(hide=True)
+                item.setSizeHint(widget.sizeHint())
+        
+    def removeRow(self):
+        row = self.ui.list_items.currentRow()
+        self.ui.list_items.takeItem(row)
+              
         
             
             
